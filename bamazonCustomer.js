cbbb67a5e80c2c7fde71a1bb8ef02aa6a573a8ef
bl3d <mr.brentlewis@gmail.com>
2017-04-20 18:14:57 -0400

    work on manager js and removing supervisor - not gonna make it for this push due to work trip :(

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,100 +2,104 @@ var inquirer = require('inquirer'),
 	mysql = require('mysql');
 	require('console.table');
 
-var connection;	
+var connection;
 
 
 
 var bamCust = {
 
-	init: function(){
-		this.connectToDB();		
+	init: function() {
+		this.connectToDB();
 	},
 
-	connectToDB: function(){
+	connectToDB: function() {
 		connection = mysql.createConnection({
-		  host: "127.0.0.1",
-		  port: 3306, 
-		  user: "gt", 
-		  password: "123",
-		  database: "Bamazon"
+			host: "127.0.0.1",
+			port: 3306,
+			user: "gt",
+			password: "123",
+			database: "Bamazon"
 		});
 
 		connection.connect(function(err) {
-		  if (err) throw err;
-		  bamCust.displayProducts();		  	 
-		});	
+			if (err) throw err;
+			bamCust.displayProducts();
+		});
 	},
 
-	displayProducts: function(){
-	  var query = "SELECT * FROM products";
-	  connection.query(query, function(err, res) {
-	    console.table(res);
-	    bamCust.getProduct(); 
-	  });		
+	displayProducts: function() {
+		var query = "SELECT * FROM products";
+		connection.query(query, function(err, res) {
+			console.table(res);
+			bamCust.getProduct();
+		});
 	},
 
-	getProduct: function(){
+	getProduct: function() {
 		inquirer.prompt([{
 			type: "input",
 			message: "What product would you like to order (item_id)?",
 			name: "id"
 		}]).then(function(feedback) {
-		    var query = "SELECT * FROM products WHERE ?";
-		    connection.query(query, { item_id: feedback.id }, function(err, res) {
-		      if (!err) {
-		      	if (res.length > 0) {
-					bamCust.getQuantity(feedback.id);      		
-		      	} else {
-		      		console.log('Sorry, that is not an item_id in the system, please try again.');
-		      		bamCust.getProduct();
-		      	}
-		      } else {
-		      	console.log('Sorry, there was an error processing your request.');
-		      	process.exit();
-		      }
-		    });
-		});		
+			var query = "SELECT * FROM products WHERE ?";
+			connection.query(query, {
+				item_id: feedback.id
+			}, function(err, res) {
+				if (!err) {
+					if (res.length > 0) {
+						bamCust.getQuantity(feedback.id);
+					} else {
+						console.log('Sorry, that is not an item_id in the system, please try again.');
+						bamCust.getProduct();
+					}
+				} else {
+					console.log('Sorry, there was an error processing your request.');
+					process.exit();
+				}
+			});
+		});
 	},
 
-	getQuantity: function(id){
+	getQuantity: function(id) {
 		inquirer.prompt([{
 			type: "input",
 			message: "How many units do you want to buy?",
 			name: "quantity"
 		}]).then(function(feedback) {
-		    var query = "SELECT stock_quantity FROM products WHERE ?";
-		    connection.query(query, { item_id: id }, function(err, res) {
-		    	var availStock = res[0].stock_quantity;
-		    	if (availStock <= 0) {
-		    		console.log('Sorry, this item is currently on backorder. Please select another product.');
-		    		bamCust.getProduct();
-		    	} else if (availStock > feedback.quantity) {
-		      	    bamCust.orderProduct(id, feedback.quantity, availStock);
-		        } else {
-		      	    console.log('Insufficient quantity!');
-		      	    bamCust.getQuantity();
-		        }
-		    });				
-		});		
+			var query = "SELECT stock_quantity FROM products WHERE ?";
+			connection.query(query, {
+				item_id: id
+			}, function(err, res) {
+				var availStock = res[0].stock_quantity;
+				if (availStock <= 0) {
+					console.log('Sorry, this item is currently on backorder. Please select another product.');
+					bamCust.getProduct();
+				} else if (availStock > feedback.quantity) {
+					bamCust.orderProduct(id, feedback.quantity, availStock);
+				} else {
+					console.log('Insufficient quantity!');
+					bamCust.getQuantity();
+				}
+			});
+		});
 	},
 
-	orderProduct: function(id, quantity, availStock){
-	    connection.query("UPDATE products set ? where ?", [{
-          stock_quantity: (availStock - quantity)
-        }, {
-          item_id: id
-        }], function(err) {
-          if (err) {
-          	  console.log(err);
-          } else {
-	          console.log("Congrats! You are now the proud owner of " + quantity + " items!");
-	          bamCust.displayProducts();          	
-          }
-        });
+	orderProduct: function(id, quantity, availStock) {
+		connection.query("UPDATE products set ? where ?", [{
+			stock_quantity: (availStock - quantity)
+		}, {
+			item_id: id
+		}], function(err) {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log("Congrats! You are now the proud owner of " + quantity + " items!");
+				bamCust.displayProducts();
+			}
+		});
 	}
 
 }
 
 
-bamCust.init();	
+bamCust.init();
--- bamazonManager.js
+++ bamazonManager.js
@@ -2,7 +2,7 @@ var inquirer = require('inquirer'),
 	mysql = require('mysql');
 	require('console.table');
 
-var connection;	
+var connection;
 
 
 
@@ -10,27 +10,27 @@ var bamMan = {
 
 	connection: null,
 
-	init: function(){
-		this.connectToDB();		
+	init: function() {
+		this.connectToDB();
 	},
 
-	connectToDB: function(){
+	connectToDB: function() {
 		connection = mysql.createConnection({
-		  host: "127.0.0.1",
-		  port: 3306, 
-		  user: "gt", 
-		  password: "123",
-		  database: "Bamazon"
+			host: "127.0.0.1",
+			port: 3306,
+			user: "gt",
+			password: "123",
+			database: "Bamazon"
 		});
 
 		connection.connect(function(err) {
-		  if (err) throw err;
-		  console.log("connected as id " + connection.threadId);
-		  bamMan.promptUser(); 	  
-		});							
+			if (err) throw err;
+			console.log("connected as id " + connection.threadId);
+			bamMan.promptUser();
+		});
 	},
 
-	promptUser: function(){
+	promptUser: function() {
 		inquirer.prompt([{
 			type: "list",
 			message: "Hello Manager, what would you like to do?",
@@ -47,131 +47,137 @@ var bamMan = {
 			var choiceIndex = parseInt(user.action.split(')'));
 
 			//
-			switch (choiceIndex){
+			switch (choiceIndex) {
 
 				case 1:
-					bamMan.view_products();					
-					break; 
+					bamMan.view_products();
+					break;
 
 				case 2:
-					bamMan.view_lowInventory();				
-					break; 
+					bamMan.view_lowInventory();
+					break;
 
 				case 3:
-					bamMan.add_toInventory();				
-					break; 
+					bamMan.add_toInventory();
+					break;
 
 				case 4:
-					bamMan.add_newProduct();				
-					break;  
+					bamMan.add_newProduct();
+					break;
 			};
-		});		
+		});
 	},
 
-	view_products: function(){
-	  var query = "SELECT * FROM products";
-	  connection.query(query, function(err, res) {
-	    console.table(res);
-	    bamMan.promptUser(); 
-	  });		
+	view_products: function() {
+		var query = "SELECT * FROM products";
+		connection.query(query, function(err, res) {
+			console.table(res);
+			bamMan.promptUser();
+		});
 	},
 
-	view_lowInventory: function(){
-	  var query = "SELECT * from products where stock_quantity <= 5";
-	  connection.query(query, function(err, res) {
-	    console.table(res);
-	    bamMan.promptUser(); 
-	  });		
+	view_lowInventory: function() {
+		var query = "SELECT * from products where stock_quantity <= 5";
+		connection.query(query, function(err, res) {
+			console.table(res);
+			bamMan.promptUser();
+		});
 	},
 
-	add_toInventory: function(){
-		// bamMan.view_products();
+	add_toInventory: function() {
+		var query = "SELECT * FROM products";
+		connection.query(query, function(err, res) {
+			inquirer.prompt([{
+				type: "list",
+				choices: function() {
+					var choiceArray = [];
+					for (var i = 0; i < res.length; i++) {
+						choiceArray.push(res[i].product_name);
+					}
+					return choiceArray;
+				},
+				message: "What product would you like to add inventory to?",
+				name: "name"
+			}]).then(function(feedback) {
+				var query = "SELECT * FROM products WHERE ?";
+				connection.query(query, {
+					product_name: feedback.name
+				}, function(err, res) {
+					var thisId = res[0].item_id;
+					var curQuantity = res[0].stock_quantity;
+					if (!err) {
+						inquirer.prompt([{
+							type: "input",
+							message: "Enter a number for how many units you wish to add:",
+							name: "quantity"
+						}]).then(function(feedback) {
+							var newQuantity = (parseInt(curQuantity) + parseInt(feedback.quantity));
+							connection.query("UPDATE products SET ? WHERE ?", [{
+								stock_quantity: newQuantity
+							}, {
+								item_id: thisId
+							}], function(error) {
+								if (error) throw err;
+								console.log("Product Updated to " + newQuantity + " units!");
+								bamMan.promptUser();
+							});
+						});
+					} else {
+						console.log('Sorry, there was an error processing your request.');
+						process.exit();
+					}
+				});
+			});
+		});
+	},
+
+	add_newProduct: function() {
 		inquirer.prompt([{
 			type: "input",
-			message: "What product would you like to add inventory to (item_id)?",
-			name: "id"
-		}]).then(function(feedback) {
-		    var query = "SELECT * FROM products WHERE ?";
-		    connection.query(query, { item_id: feedback.id }, function(err, res) {
-		      var thisId = feedback.id;
-		      var curQuantity = res[0].stock_quantity; 
-		      if (!err) {
-		      	if (res.length > 0) {
-					inquirer.prompt([{
-						type: "input",
-						message: "Enter a number for how many units you wish to add:",
-						name: "quantity"
-					}]).then(function(feedback) { 
-						var newQuantity = (parseInt(curQuantity) + parseInt(feedback.quantity));
-				        connection.query("UPDATE products SET ? WHERE ?", [{
-				          stock_quantity: newQuantity
-				        }, {
-				          item_id: thisId
-				        }], function(error) {
-				          if (error) throw err;
-				          console.log("Product Updated to " + newQuantity + " units!");
-				          bamMan.promptUser();
-				        });						
-					});	    		
-		      	} else {
-		      		console.log('Sorry, that is not an item_id in the system, please try again.');
-		      		bamMan.getProduct();
-		      	}
-		      } else {
-		      	console.log('Sorry, there was an error processing your request.');
-		      	process.exit();
-		      }
-		    });
-		});			
-	},
-
-	add_newProduct: function(){
-	  inquirer.prompt([{	    
-	    type: "input",
-	    message: "What is the product you wish to add?",
-	    name: "name"
-	  }, {
-	    type: "input",
-	    message: "What department is this product associated with?",
-	    name: "dept"
-	  }, {
-	    type: "input",
-	    message: "What is the retail cost of this item (per unit)?",
-	    name: "price",
-	    validate: function(value) {
-	      if (isNaN(value) === false) {
-	        return true;
-	      }
-	      console.log("\nThis needs to be a number...");
-	      return false;
-	    }
-	  }, {
-	    type: "input",
-	    message: "How many of these items are being added into stock?",
-	    name: "quantity",
-	    validate: function(value) {
-	      if (isNaN(value) === false) {
-	        return true;
-	      }
-	      console.log("\nThis needs to be a number...");
-	      return false;
-	    }
-	  }]).then(function(product) {
-	    // when finished prompting, insert a new item into the db with that info
-	    connection.query("INSERT INTO products SET ?", {
-	      product_name: product.name,
-	      department_name: product.dept,
-	      price: product.price,
-	      stock_quantity: product.quantity
-	    }, function(err) {
-	      if (err) throw err;
-	      console.log("Your product '" + product.name + "' was added successfully!");
-	      bamMan.promptUser();
-	    });
-	  });		
+			message: "What is the product you wish to add?",
+			name: "name"
+		}, {
+			type: "input",
+			message: "What department is this product associated with?",
+			name: "dept"
+		}, {
+			type: "input",
+			message: "What is the retail cost of this item (per unit)?",
+			name: "price",
+			validate: function(value) {
+				if (isNaN(value) === false) {
+					return true;
+				}
+				console.log("\nThis needs to be a number...");
+				return false;
+			}
+		}, {
+			type: "input",
+			message: "How many of these items are being added into stock?",
+			name: "quantity",
+			validate: function(value) {
+				if (isNaN(value) === false) {
+					return true;
+				}
+				console.log("\nThis needs to be a number...");
+				return false;
+			}
+		}]).then(function(product) {
+			// when finished prompting, insert a new item into the db with that info
+			connection.query("INSERT INTO products SET ?", {
+				product_name: product.name,
+				department_name: product.dept,
+				price: product.price,
+				stock_quantity: product.quantity
+			}, function(err) {
+				if (err) throw err;
+				console.log("Your product '" + product.name + "' was added successfully!");
+				bamMan.promptUser();
+			});
+		});
 	}
 
 }
 
 
-bamMan.init();	
+bamMan.init();
